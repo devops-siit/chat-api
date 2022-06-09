837b0f682eee7d644ad0a56fc36155face2c97b2
tamaraglisic <39412716+tamaraglisic@users.noreply.github.com>
2022-06-09 22:10:33 +0200

    Setup/init database (#2)
    * feat(setup): configured database for accounts in docker compose
    
    * fix(setup): hostname in jdbc connection changed
    
    * feat(setup): configured database
    
    * feat(setup): configured database for offers in docker compose
    
    * feat(setup): configured database for posts in docker compose
    
    * feat(setup): added build in dockerfile, removed docker-compose
    
    * feat(setup): configure database for tests
    
    Co-authored-by: Petar <quadrition@gmail.com>

--- src/test/java/com/dislinkt/chatapi/ChatApiApplicationTests.java
+++ src/test/java/com/dislinkt/chatapi/ChatApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.dislinkt.chatapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ChatApiApplicationTests {
 
 	@Test

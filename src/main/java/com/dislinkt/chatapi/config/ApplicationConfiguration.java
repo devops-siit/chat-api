3717a22958ce5caf3bc2c53f7fbf41ba21819900
Petar Cerović <quadrition@gmail.com>
2022-09-10 23:39:00 +0200

    feat: added security

--- src/main/java/com/dislinkt/chatapi/config/ApplicationConfiguration.java
+++ src/main/java/com/dislinkt/chatapi/config/ApplicationConfiguration.java
@@ -3,6 +3,7 @@ package com.dislinkt.chatapi.config;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class ApplicationConfiguration {
@@ -10,5 +11,10 @@ public class ApplicationConfiguration {
     @Bean
     public Jackson2JsonMessageConverter converter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
